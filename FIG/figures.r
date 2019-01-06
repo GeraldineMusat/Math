2c95e5c0bea2daa75f2da22e241b842d31c0db55
romainpace <r-pace@hotmail.fr>
2019-01-06 23:24:40 +0100

    Modif fichier figures.r

--- FIG/figures.r
+++ FIG/figures.r
@@ -81,33 +81,90 @@ p <- ggplot(data=newyork_long_temp, aes(x = Long, y = Lat, fill = ..level..)) +
   labs(fill = "Densite")
 # print(p)
 
+# NewYork 
+plot(newyork_stat[,"Date"], newyork_stat[,"Moy"], type="b") 
+plot(newyork_stat[,"Date"], newyork_stat[,"Min"], type="b")
+plot(newyork_stat[,"Date"], newyork_stat[,"Max"], type="b")
+plot(newyork_stat[,"Date"], newyork_stat[,"EcType"], type="b") 
+
+# Couverture des donnees recoltees sur la Floride
+p <- ggplot(data=newyork_long_temp, aes(x = Long, y = Lat, fill = ..level..)) +
+  stat_density_2d(geom = "polygon") +
+  xlab("Longitude") +
+  ylab("Latitude") +
+  labs(fill = "Densite")
+# print(p)
+
 
 # MONTANA
 # Ete
 tab <- montana_stat_saison[montana_stat_saison$Saison == "Ete",]
-plot(tab[,"Date"], tab[,"EcType"], type="b") 
+plot(tab[,"Date"], tab[,"EcType"], xlab="Années", ylab="Températures" , main="Evolution de la température au Montana en été", type="b") 
+# Hiver
+tab <- montana_stat_saison[montana_stat_saison$Saison == "Hiver",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température au Montana en hiver", type="b")
+# Automne
+tab <- montana_stat_saison[montana_stat_saison$Saison == "Automne",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température au Montana en automne", type="b")
+#Printemps
+tab <- montana_stat_saison[montana_stat_saison$Saison == "Printemps",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température au Montana au printemps", type="b")
 
 
 # Californie
 # Ete
 tab <- californi_stat_saison[californi_stat_saison$Saison == "Ete",]
-plot(tab[,"Date"], tab[,"Moy"], type="b")
-
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température en Californie en été", type="b")
+# Hiver
+tab <- californi_stat_saison[californi_stat_saison$Saison == "Hiver",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température en Californie en hiver", type="b")
+# Automne
+tab <- californi_stat_saison[californi_stat_saison$Saison == "Automne",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température en Californie en automne", type="b")
+#Printemps
+tab <- californi_stat_saison[californi_stat_saison$Saison == "Printemps",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température en Californie au Printemps", type="b")
 
 # Kansas
 # Ete
 tab <- kansas_stat_saison[kansas_stat_saison$Saison == "Ete",]
-plot(tab[,"Date"], tab[,"Moy"], type="b")
+plot(tab[,"Date"], tab[,"Moy"], xlab="Années", ylab="Températures" , main="Evolution de la température au Kansas en été", type="b")
+# Hiver
+tab <- kansas_stat_saison[kansas_stat_saison$Saison == "Hiver",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température au Kansas en hiver", type="b")
+# Automne
+tab <- kansas_stat_saison[kansas_stat_saison$Saison == "Automne",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température au Kansas en automne", type="b")
+#Printemps
+tab <- kansas_stat_saison[kansas_stat_saison$Saison == "Printemps",]
+plot(tab[,"Date"], tab[,"Moy"], xlab="Années", ylab="Températures" , main="Evolution de la température au Kansas au printemps",type="b")
 
 
 # New York
 # Ete
 tab <- newyork_stat_saison[newyork_stat_saison$Saison == "Ete",]
-plot(tab[,"Date"], tab[,"Moy"], type="b")
+plot(tab[,"Date"], tab[,"Moy"], xlab="Années", ylab="Températures" , main="Evolution de la température à New York en été",type="b")
+# Hiver
+tab <- newyork_stat_saison[newyork_stat_saison$Saison == "Hiver",]
+plot(tab[,"Date"], tab[,"Moy"], xlab="Années", ylab="Températures" , main="Evolution de la température à New York en hiver",type="b")
+# Automne
+tab <- newyork_stat_saison[newyork_stat_saison$Saison == "Automne",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température à New York en automne", type="b")
+#Printemps
+tab <- newyork_stat_saison[newyork_stat_saison$Saison == "Printemps",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température à New York au printemps", type="b")
 
 
 # Floride
 # Ete
 tab <- floride_stat_saison[floride_stat_saison$Saison == "Ete",]
-plot(tab[,"Date"], tab[,"Moy"], type="b")
-
+plot(tab[,"Date"], tab[,"Moy"], xlab="Années", ylab="Températures" , main="Evolution de la température en Floride en été",type="b")
+# Hiver
+tab <- floride_stat_saison[floride_stat_saison$Saison == "Hiver",]
+plot(tab[,"Date"], tab[,"Moy"], xlab="Années", ylab="Températures" , main="Evolution de la température en Floride en hiver", type="b")
+# Automne
+tab <- floride_stat_saison[floride_stat_saison$Saison == "Automne",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température en Floride en automne", type="b")
+#Printemps
+tab <- floride_stat_saison[floride_stat_saison$Saison == "Printemps",]
+plot(tab[,"Date"], tab[,"Moy"],xlab="Années", ylab="Températures" , main="Evolution de la température en Floride au printemps", type="b")
